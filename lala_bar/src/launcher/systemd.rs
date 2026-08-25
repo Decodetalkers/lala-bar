e2e425d0273021401af5600472b1c5901fadddd9
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-08-25 16:51:48 +0900

    chore: remove unused code

--- lala_bar/src/launcher/systemd.rs
+++ lala_bar/src/launcher/systemd.rs
@@ -6,24 +6,7 @@ use zbus::{
 };
 
 use serde::{Deserialize, Serialize};
-use std::hash::Hash;
 
-use std::sync::atomic::{self, AtomicU64};
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord)]
-/// The id of the window.
-///
-/// Internally Iced reserves `window::Id::MAIN` for the first window spawned.
-pub struct Id(u64);
-
-static COUNT: AtomicU64 = AtomicU64::new(0);
-
-impl Id {
-    /// Creates a new unique window [`Id`].
-    pub fn unique() -> Id {
-        Id(COUNT.fetch_add(1, atomic::Ordering::Relaxed))
-    }
-}
 #[derive(Debug, Serialize, Deserialize, Type, Clone, Copy)]
 #[serde(rename_all = "lowercase")]
 #[zvariant(signature = "s")]
