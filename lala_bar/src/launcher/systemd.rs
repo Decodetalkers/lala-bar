1e2c1be1b277ab6c1b8b9c191e07d477bb3db2c3
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2026-08-25 16:04:35 +0900

    fix: incorrect scope name
    reference: https://github.com/flatpak/xdg-desktop-portal/blob/main/shared/xdp-app-info-host.c#L111-L113

--- lala_bar/src/launcher/systemd.rs
+++ lala_bar/src/launcher/systemd.rs
@@ -142,7 +142,7 @@ pub async fn launch(id: &str, cmd: &[String], description: &str) -> anyhow::Resu
         .destination("org.freedesktop.systemd1")?
         .build()
         .await?;
-    let service = format!("lala-bar-tmp.{id}@{}.service", Id::unique().0);
+    let service = format!("app-lalabar-{id}@{}.service", Id::unique().0);
 
     systemd
         .start_transient_unit(
